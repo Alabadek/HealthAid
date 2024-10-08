4a94468a1e218a80b23ac2fb9b44338b42d5abd4
Jochebed Opoku <jayb024417@gmail.com>
2024-10-08 18:12:53 +0000

    some minor changes

--- Health/obj/Debug/net8.0/Health.AssemblyInfo.cs
+++ Health/obj/Debug/net8.0/Health.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Health")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c81fa9c9d55bda59c2cf878b48da1fbe8de398")]
 [assembly: System.Reflection.AssemblyProductAttribute("Health")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Health")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
